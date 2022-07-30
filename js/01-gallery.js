662b4659fba6e2ae70d031166e7f6a34ad4327de
Alex Dolhov <alexdolhov@gmail.com>
2022-07-30 22:55:06 +0300

    adds code
    - adds code to galary.html
    - adds code to galary.js
    - adds code to lightbox.html
    - adds code to lightbox.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+// const items = galleryItems;
+
+// console.log(items);
+
+const galaryContainer = document.querySelector(".gallery");
+
+const galaryMarkup = createGalaryMarkup(galleryItems);
+
+galaryContainer.insertAdjacentHTML("beforeend", galaryMarkup);
+galaryContainer.addEventListener("click", onGalaryContainerClick);
+
+function createGalaryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+            </a>
+        </div>`;
+    })
+    .join("");
+}
+
+function onGalaryContainerClick(e) {
+  e.preventDefault();
+
+  // console.dir(e.target);
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const onClickedImageUrl = e.target.dataset.source;
+
+  const instance = basicLightbox.create(`<img src ="${onClickedImageUrl}"/>`);
+  instance.show();
+
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galaryContainer = document.querySelector(".gallery");
+
+const galaryMarkup = createGalaryMarkup(galleryItems);
+
+galaryContainer.insertAdjacentHTML("beforeend", galaryMarkup);
+// galaryContainer.addEventListener("click", ongalaryContainerClick);
+
+function createGalaryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+            <a class="gallery__item" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+            </a>
+            `;
+    })
+    .join("");
+}
+
+let gallery = new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
